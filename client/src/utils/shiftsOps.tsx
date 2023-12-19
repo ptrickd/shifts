ae2710cff0e65946d27bc204f239167e5e11f2ba
ptrickd <patrickdion8@gmail.com>
2023-12-19 05:33:31 -0700

    New shift get id from backend

--- client/src/utils/shiftsOps.tsx
+++ client/src/utils/shiftsOps.tsx
@@ -23,44 +23,44 @@ const updateShifts: (
       return { ...shift };
     }
   });
-
-  if (!foundShift) {
-    dispatch({
-      type: ACTIONS.ADD_SHIFT,
-      payload: {
-        id,
+  const updatingShift = async () => {
+    if (!foundShift) {
+      const response = await postShift({
         employeeId,
         startTime,
         endTime,
         date,
         weekStart,
-      },
-    });
+      });
+      dispatch({
+        type: ACTIONS.ADD_SHIFT,
+        payload: {
+          id: response.id,
+          employeeId,
+          startTime,
+          endTime,
+          date,
+          weekStart,
+        },
+      });
+    } else if (foundShift) {
+      dispatch({
+        type: ACTIONS.UPDATE_SHIFT,
+        payload: {
+          id,
+          employeeId,
+          startTime,
+          endTime,
+          date,
+          weekStart,
+        },
+      });
 
-    postShift({
-      employeeId: employeeId,
-      startTime: startTime,
-      endTime: endTime,
-      date: date,
-      weekStart,
-    });
-  } else if (foundShift) {
-    dispatch({
-      type: ACTIONS.UPDATE_SHIFT,
-      payload: {
-        id,
-        employeeId,
-        startTime,
-        endTime,
-        date,
-        weekStart,
-      },
-    });
+      putShift(foundShift);
+    } else throw new Error("founshfit not found");
+  };
+  updatingShift();
 
-    putShift(foundShift);
-  } else throw new Error("founshfit not found");
-
-  // fetch;
   return newShifts;
 };
 
